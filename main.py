8e74f6aca4c2023abf2064b512ea4fedaa96bb54
ShellockOps <218080613+shellockops@users.noreply.github.com>
2025-07-13 16:33:42 +0200

    Add a function to check pwned database

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pypass
 
 def main():
-    #password = pypass.gen_password()
+    password = "love2d"   #pypass.gen_password()
     #print(password)
-    entropy = pypass.find_entropy("1Bankruptcies2&%")
-    print(entropy)
+    entropy = pypass.find_entropy(password)
+
+    print("The entropy of your password is: ", entropy)
+    occurency = pypass.check_db(password)
+
+    if occurency:
+        print(f"This password has been seen {occurency:,} times before in data breaches!\nChange it immediately!!!")
+    else:
+        print("This password wasn't found in any of the Pwned Passwords loaded into Have I Been Pwned.\nThat doesn't necessarily mean it's a good password, merely that it's not indexed on this site.")
+
 main()
--- pypass.py
+++ pypass.py
@@ -1,9 +1,11 @@
 import string
 import secrets
 from math import log2, floor
+import hashlib
+import requests
 
 def gen_password():
-    size = 20
+    default_size = 20
 
     total = []
     total.append(string.ascii_lowercase)
@@ -14,7 +16,7 @@ def gen_password():
     password = ""
 
     count = 0
-    for i in range(21):
+    for i in range(default_size + 1):
         password += secrets.choice(total[count])
         if count < 3:
             count += 1
@@ -22,7 +24,7 @@ def gen_password():
             count = 0
 
     mix = list(password)
-    secrets.shuffle(mix)
+    secrets._sysrand.shuffle(mix)
 
     return "".join(mix)
 
@@ -56,3 +58,26 @@ def find_entropy(password):
     entropy = floor(entropy * 10) / 10
     
     return entropy
+
+
+def check_db(password):
+
+    sha1_pass = hashlib.sha1(password.encode()).hexdigest()
+
+    url = f"https://api.pwnedpasswords.com/range/{sha1_pass[:5]}"
+    sha1_postfix = sha1_pass[5:].upper()
+
+    response = requests.request("GET", url)
+    pwned_list = response.text.split("\n")
+    hash_dict = {}
+
+    for pwned_pass in pwned_list:
+        pwned_hash_times = pwned_pass.split(":")
+        hash_dict[pwned_hash_times[0]] = pwned_hash_times[1]
+    
+    count = 0
+    for key, value in hash_dict.items():
+        if sha1_postfix in key:
+            count += int(value)
+    
+    return count
